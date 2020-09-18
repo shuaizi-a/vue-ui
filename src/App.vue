c791008b84277853ee4009888e683481493d3330
shuaizi <77367582@qq.com>
2020-09-18 10:46:03 +0800

    树形控件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div>Switch(开关示例)</div>
     <sz-Switch :checked="bool" @input="toggon($event)"></sz-Switch>
     <br />
     <sz-Switch disabled></sz-Switch>
@@ -10,7 +11,9 @@
     <sz-Button type="info">信息</sz-Button>
     <sz-Button type="warning">警告</sz-Button>
     <sz-Button type="danger">危险</sz-Button>
+
     <br />
+    <div>input框示例</div>
     <sz-Input v-model="lovingVue" placeholder="这是设置信息" clearable></sz-Input>
     <sz-Input v-model="lovingVue2" placeholder="这是设置信息" disabled></sz-Input>
 
@@ -69,6 +72,10 @@
       <sz-Button2 loading>加载中</sz-Button2>
       <sz-Button2>加载完毕</sz-Button2>
     </div>
+
+    <br />
+    <div>树形控件</div>
+    <sz-Thetree :data="trees" @select="Thetree"></sz-Thetree>
   </div>
 </template>
 
@@ -81,6 +88,118 @@ export default {
       bool2: false,
       lovingVue: "",
       lovingVue2: "输入框2",
+      trees: [
+        {
+          id: 1,
+          label: "1级目录1",
+          show: false,
+          children: [
+            {
+              id: "1-1",
+              label: "1.1目录",
+            },
+            {
+              id: "1-2",
+              label: "1.2目录",
+            },
+            {
+              id: "1-3",
+              label: "1.3目录",
+            },
+          ],
+        },
+        {
+          id: 2,
+          label: "1级目录2",
+          show: false,
+        },
+        {
+          id: 3,
+          label: "1级目录3",
+          show: false,
+          children: [
+            {
+              id: "3-1",
+              label: "3.1目录",
+            },
+            {
+              id: "3-2",
+              label: "3.2目录",
+              show: false,
+              children: [
+                {
+                  id: "3-2-1",
+                  label: "3.2.1目录",
+                },
+                {
+                  id: "3-2-2",
+                  label: "3.2.2目录",
+                },
+                {
+                  id: "3-2-3",
+                  label: "3.2.3目录",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 4,
+          label: "1级目录4",
+          show: false,
+          children: [
+            {
+              id: "4-1",
+              label: "4.1目录",
+            },
+            {
+              id: "4-2",
+              label: "4.2目录",
+              show: false,
+              children: [
+                {
+                  id: "4-2-1",
+                  label: "4.2.1目录",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: 5,
+          label: "1级目录5",
+          show: false,
+          children: [
+            {
+              id: "5-1",
+              label: "5.1目录",
+              show: false,
+              children: [
+                {
+                  id: "5-1-1",
+                  label: "5.1.1目录",
+                },
+                {
+                  id: "5-1-2",
+                  label: "5.1.2目录",
+                  show: false,
+                  children: [
+                    {
+                      id: "5-1-2-1",
+                      label: "5.1.2.1目录",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              id: "5-2",
+              label: "5.2目录",
+              show: false,
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
@@ -91,6 +210,9 @@ export default {
     onClick() {
       console.log(1);
     },
+    Thetree(item){
+      console.log(item)
+    }
   },
   watch: {
     lovingVue(x, y) {
---
+++ src/lib/Thetree.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li v-for="item in data" :key="item.id">
+      <div>
+        <img
+          src="../img/wjjk.png"
+          alt="关闭"
+          title="关闭"
+          @click="item.show = !item.show"
+          v-if="item.children && item.show"
+        />
+        <img
+          src="../img/wjjg.png"
+          alt="打开"
+          title="打开"
+          @click="item.show = !item.show"
+          v-if="item.children && !item.show"
+        />
+        <span :class="{pad:!item.children}" @click="select(item)">{{ item.label}}</span>
+      </div>
+      <sz-Thetree
+        class="pad"
+        v-show="item.children && item.show"
+        :data="item.children"
+        @select="select"
+      ></sz-Thetree>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "sz-Thetree",
+  props: {
+    data: {
+      type: Array,
+    },
+  },
+  methods: {
+    select(item) {
+      this.$emit("select", item);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  padding-left: 0px;
+}
+li {
+  list-style-type: none;
+}
+.pad {
+  padding-left: 24px;
+}
+</style>
